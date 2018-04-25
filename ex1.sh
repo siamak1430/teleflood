24863dbb67b77b123346bef78a854e51286ecaf8
siamak1430 <siamak.hidary1372@gmail.com>
2018-04-25 23:03:16 +0000

    initial commit

--- ex1.sh
+++ ex1.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-OF=myhome_directory_$(date +%Y%m%d).tar.gz
-tar -czf $OF /home/blackmac/
+
+function get_window_id () {
+    value= xdotool search --name "Telegram "
+    return $value
+}
+
+window_id=$(get_window_id)
+
+if [ -z $window_id ];
+then 
+    echo "Error: run telegram and try again"
+    exit
+fi
+
+for ((i=1; i<=1000; i++))
+do
+    xdotool type --window $window_id "xxxxxxxxxxxxxxxxxxxxx"
+    xdotool windowactivate $window_id
+    xdotool key Return 
+done
+exit
